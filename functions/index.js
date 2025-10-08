5b6984372d45724fba7888ebee8d2d23414ee71e
grantwhitburn <grantwhitburn@gmail.com>
2025-10-08 10:43:53 +0000

    Fix function and remove log

--- functions/index.js
+++ functions/index.js
@@ -3,7 +3,3 @@ const functions = require('firebase-functions');
 exports.helloWorld = functions.https.onRequest((request, response) => {
   response.send("Hello from Firebase!");
 });
-
-exports.onInit = functions.https.onInit(() => {
-  // No-op
-});
